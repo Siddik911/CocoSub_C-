f7e32cd7a82df70a9e6699c206a73f1a8508d6f0
Adnan Hossain Ratul <129319701+adnan2248@users.noreply.github.com>
2024-09-27 00:37:39 +0600

    end of cusotmer

---
+++ cocosubbetaversion/succesfully_recived_order.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace cocosubbetaversion
+{
+    partial class succesfully_recived_order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GO_HOMRPAGE_BUTTON = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // GO_HOMRPAGE_BUTTON
+            // 
+            this.GO_HOMRPAGE_BUTTON.AutoRoundedCorners = true;
+            this.GO_HOMRPAGE_BUTTON.BackColor = System.Drawing.Color.Transparent;
+            this.GO_HOMRPAGE_BUTTON.BorderRadius = 21;
+            this.GO_HOMRPAGE_BUTTON.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.GO_HOMRPAGE_BUTTON.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.GO_HOMRPAGE_BUTTON.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.GO_HOMRPAGE_BUTTON.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.GO_HOMRPAGE_BUTTON.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(56)))), ((int)(((byte)(51)))));
+            this.GO_HOMRPAGE_BUTTON.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GO_HOMRPAGE_BUTTON.ForeColor = System.Drawing.Color.White;
+            this.GO_HOMRPAGE_BUTTON.Location = new System.Drawing.Point(380, 540);
+            this.GO_HOMRPAGE_BUTTON.Name = "GO_HOMRPAGE_BUTTON";
+            this.GO_HOMRPAGE_BUTTON.Size = new System.Drawing.Size(230, 45);
+            this.GO_HOMRPAGE_BUTTON.TabIndex = 0;
+            this.GO_HOMRPAGE_BUTTON.Text = "GO TO HOMESCREEN";
+            this.GO_HOMRPAGE_BUTTON.UseTransparentBackground = true;
+            // 
+            // succesfully_recived_order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::cocosubbetaversion.Properties.Resources.C__x_CocoSub__33_;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.GO_HOMRPAGE_BUTTON);
+            this.Name = "succesfully_recived_order";
+            this.Text = "succesfully_recived_order";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button GO_HOMRPAGE_BUTTON;
+    }
+}
